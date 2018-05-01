322e9d2b3d8d64f4266511b68d9972e491be31ec
Marta Waplak <mwaplak@gmail.com>
2018-05-01 15:54:04 +0200

    Reconfigure eslint and fix remaining issues

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Title = props => <div><h1>{props.title}</h1><h3>Liczba zadań: {props.lenght}</h3></div>;
+
+Title.propTypes = {
+	title: PropTypes.string,
+	lenght: PropTypes.number
+};
+
 
 export default Title;
